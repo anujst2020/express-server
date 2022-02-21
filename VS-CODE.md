657dfc8481bb58b276d85958093fd2b5c876ac9f
Anuj Chauhan <anuj.chauhan@anujR231.successivetech.com>
2022-02-21 19:08:16 +0530

    pattern done

---
+++ VS-CODE.md
@@ -0,0 +1,21 @@
+Plugins of VS code
+
+Prettier
+Debugger for Chrome
+ESLint
+Bracket Pair Colorizer
+GitLens
+Regex Previewer
+Auto Rename Tag
+Beautify
+
+
+
+Features of VSCode
+
+Many shortcuts for copy/select/move vertically etc
+The Integrated CLI
+Command Palette
+Split view
+Git Integration
+Zen Mode(for full screen)
---
+++ extra/patterns/diamond.js
@@ -0,0 +1,22 @@
+const args = process.argv.slice(2);
+let rows = args[0];
+
+if(rows == undefined){
+    console.error('Please pass argument');
+}else if(rows != undefined && (rows <2 || rows > 10)){
+    console.error('Please pass argument between 2 to 10');
+}else{
+    for(let i= 1; i<= 2*rows-1; i++){
+        let star = '';
+        let allign = '';
+        let loop = (i > rows)? 2*rows-i: i;
+        for(let j= 0; j< loop; j++){
+            star += '* ';
+        }
+        star = star.trim();
+        for(let k= rows; k>= loop; k--){
+            allign += ' ';
+        }
+        console.log(allign+star);
+    }
+}
