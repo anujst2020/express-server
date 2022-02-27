4cc195e725f8ba09d252a73bb51820f3fcd7b93e
Anuj Chauhan <anuj.chauhan@anujR231.successivetech.com>
2022-02-27 17:59:28 +0530

    sort and limit skip

--- src/controllers/trainee/Controller.ts
+++ src/controllers/trainee/Controller.ts
@@ -1,25 +1,66 @@
+import UserModel  from '../../repositories/user/UserModel';
+
 class Trainee {
+
     public static getTrainees(req, res){
-        return res.status(200).send({'message': 'get method of trainee controller'});
+        let limit = req.query.limit? req.query.limit: 10;
+        let skip = req.query.page? (parseInt(req.query.page)-1)*limit: 0;
+        let search_name = req.query.name;
+        let search_email = req.query.email;
+        let search_obj = {};
+        if(search_email){
+            search_obj = {email: {$regex: search_email, $options: 'i'}};
+        }else if(search_name){
+            search_obj = {first_name: {$regex: search_name, $options: 'i'}};
+        }else if(search_name && search_email){
+            search_obj = {first_name: {$regex: search_name, $options: 'i'}, email: {$regex: search_email, $options: 'i'}};
+        }
+
+        var query = UserModel.find(search_obj, {password: 0, _id: 0, __v: 0}).skip(skip).limit(limit).sort({email: 1});
+        query.exec((err, users)=>{
+            if(err)
+                return res.status(500).send({data:[], 'message': err.message});
+            return res.status(200).send({data: users, 'message': 'trainees fetched successfully'});
+        });
     }
+
     public static getTrainee(req, res){
-        return res.status(200).send({'message': 'get method of trainee controller'});
+        UserModel.findOne({_id: req.params.id},{password: 0},(err, users)=>{
+            if(err)
+                return res.status(500).send({data:[], 'message': err.message});
+            return res.status(200).send({data: users, 'message': 'trainee fetched successfully'});
+        });
     }
 
     public static postTrainee(req, res){
         // get data from body and perform task
-        return res.status(200).send({'message': 'post method of trainee controller'});
-
+        UserModel.create({},(err, user)=>{
+            if(err)
+                return res.status(500).send({'message': err.message});
+            return res.status(200).send({'message': 'trainee created successfully'});
+        });
     }
 
     public static putTrainee(req, res){
         // get data from body and perform task
-        return res.status(200).send({'message': 'put method of trainee controller'});
+        UserModel.updateOne({_id: req.body.id},
+            {
+                first_name: req.body.first_name,
+                last_name: req.body.last_name            },
+            (err, user)=>{
+                if(err)
+                    return res.status(500).send({'message': err.message});
+                return res.status(200).send({'message': 'trainee updated successfully'});
+            });
     }
 
     public static deleteTrainee(req, res){
         // Get id from param/body and perform task
-        return res.status(200).send({'message': 'delete method of trainee controller'});
+        UserModel.deleteOne({_id: req.params.id}, (err, user)=>{
+            if(err)
+                return res.status(500).send({'message': err.message});
+            return res.status(200).send({'message': 'Trainee deleted successfully'});
+        });
     }
 }
 
